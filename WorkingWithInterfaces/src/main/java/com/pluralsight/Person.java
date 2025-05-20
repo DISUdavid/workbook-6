5210b31a0df521d27a1c911051c7fb0008b8a7c9
David Disu <disudavid9@gmail.com>
2025-05-20 14:42:01 -0500

    Last Name, First Name (sort) commit

---
+++ WorkingWithInterfaces/src/main/java/com/pluralsight/Person.java
@@ -0,0 +1,33 @@
+package com.pluralsight;
+
+public class Person implements Comparable<Person> {
+
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public Person(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    @Override
+    public int compareTo(Person person) {
+        return this.lastName.compareTo(person.lastName);
+    }
+
+}
